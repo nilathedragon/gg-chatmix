732a1813541b73696374295372ee802f45b41a90
NilaTheDragon <nilathedragon@pm.me>
2025-05-23 01:37:07 +0200

    Add Flake

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Development Environment for GG Chatmix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    packageName = "gg-chatmix";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        libusb1
+        pkg-config
+        zsh
+      ];
+
+      shellHook = ''
+        echo "`go version`"
+        exec zsh
+      '';
+    };
+  
+    packages.${system} = let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.buildGoModule {
+        name = packageName;
+        src = pkgs.fetchFromGitHub {
+          owner = "nilathedragon";
+          repo = "gg-chatmix";
+          rev = "e81b3cbe7325a7dd4af8c73347d1c2fbc1ae5a1b";
+          sha256 = "sha256-V9Rg6qNRrS69I94hEaE3jAfzFLmkX8vHSR5EqDch5xY=";
+        };
+        vendorHash = "sha256-cmiC1BbBE9orgEkDpDesqP0Tf1kBW9WMIyRNtwxU1fU=";
+
+        buildInputs = with pkgs; [
+          libusb1
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
+    };
+
+    nixosModule = let 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in { self, config, lib, pkgs, ... }:
+    {
+      options.services.gg-chatmix = {
+        enable = lib.mkEnableOption "enable the gg-chatmix service";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.packages.${system}.gg-chatmix;
+          description = "gg-chatmix package to use";
+        };
+      };
+
+      config = lib.mkIf (config.services.gg-chatmix.enable) {
+        systemd.user.services.gg-chatmix = {
+          description = "GG Chatmix daemon";
+          serviceConfig = {
+            Type = "exec";
+            ExecStart = "${config.services.gg-chatmix.package}/bin/cmd";
+            Restart = "always";
+          };
+        };
+      };
+    };
+  };
+}
